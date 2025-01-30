b2f6ae6c233255e6f72399c8cbe4116d83b10854
Fay Ash <yaoshiu@qq.com>
2025-01-30 22:40:10 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            deno
+            nodejs
+            ansible
+            just
+          ];
+        };
+      }
+    );
+}
